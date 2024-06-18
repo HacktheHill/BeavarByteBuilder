ff4bbcba01a5e0a324b1772b3995d52b58e9f18a
Mumtahin Farabi <mfarabi619@gmail.com>
2024-06-17 21:28:48 -0400

    chore(SevSeg): Add init function

--- example/seven-segment-display/seven-segment-display.ino
+++ example/seven-segment-display/seven-segment-display.ino
@@ -9,9 +9,34 @@
  */
 
 #include "SevSeg.h"
-SevSeg sevseg; //Instantiate a seven segment controller object
+
+SevSeg sevseg; // Instantiate a seven segment controller object
 
 void setup() {
+  initSevSeg();
+}
+
+void loop() {
+  static unsigned long timer = millis();
+  static int deciSeconds = 0;
+  
+  if (millis() - timer >= 100) {
+    timer += 100;
+    deciSeconds++; // 100 milliSeconds is equal to 1 deciSecond
+    
+    if (deciSeconds == 10000) { // Reset to 0 after counting for 1000 seconds.
+      deciSeconds = 0;
+    }
+    sevseg.setNumber(deciSeconds, 1);
+  }
+
+  sevseg.refreshDisplay(); // Must run repeatedly
+}
+
+/**
+ * @brief Initializes the SevSeg display with the given configuration.
+ */
+void initSevSeg() {
   byte numDigits = 4;
   byte digitPins[] = {2, 3, 4, 5};
   byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
@@ -24,21 +49,4 @@ void setup() {
   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
   updateWithDelays, leadingZeros, disableDecPoint);
   sevseg.setBrightness(90);
-}
-
-void loop() {
-  static unsigned long timer = millis();
-  static int deciSeconds = 0;
-  
-  if (millis() - timer >= 100) {
-    timer += 100;
-    deciSeconds++; // 100 milliSeconds is equal to 1 deciSecond
-    
-    if (deciSeconds == 10000) { // Reset to 0 after counting for 1000 seconds.
-      deciSeconds=0;
-    }
-    sevseg.setNumber(deciSeconds, 1);
-  }
-
-  sevseg.refreshDisplay(); // Must run repeatedly
 }
